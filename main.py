4ef17f8a9d882d4b263b4aa2d2dca8babe076db8
hopo55 <hopo5798@gmail.com>
2022-09-24 13:47:17 +0900

    Frist commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import argparse
+import dataloaders
 
 def run(args):
-    pass
+    if args.dataset == 'CIFAR10':
+        Dataset = dataloaders.iCIFAR10
+    elif args.dataset == 'CIFAR100':
+        Dataset = dataloaders.iCIFAR100
+    else:
+        print('Dataset does not select.')
+        exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Semi-Supervised Continual Learning')
-    parser.add_argument()
+    parser.add_argument('--seed', type=int, default=0, help='Set seed (default=0)')
+    parser.add_argument('--dataset', type=str, default='CIFAR100', help="CIFAR10|CIFAR100")
     
     args = parser.parse_args()
 
